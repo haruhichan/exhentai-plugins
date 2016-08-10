b553bd44dc012dd97997143366feb15b377021a8
FCNG Dev <fcng.dev@gmail.com>
2016-08-10 18:04:45 +0200

    Replaced http://ex with https://ex

--- chrome/lib/background.js
+++ chrome/lib/background.js
@@ -3,7 +3,7 @@ function setHentaiCookies() {
 		chrome.cookies.getAll({domain:'.e-hentai.org'}, function(got) {
 			for(var i = 0; i < got.length; i++) {
 				if(got[i].name.indexOf('ipb_') != -1 || got[i].name.indexOf('uconfig') != -1) {
-					chrome.cookies.set({url:'http://exhentai.org/', domain:'.exhentai.org', name:got[i].name, path:'/', value:got[i].value});
+					chrome.cookies.set({url:'https://exhentai.org/', domain:'.exhentai.org', name:got[i].name, path:'/', value:got[i].value});
 				}
 			}
 		});
@@ -18,11 +18,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if(request == 'cookieDataSet') {
 		sendResponse((setHentaiCookies() ? 'ok' : 'Unable to set cookies'));
 	} else if(request == 'deleteAllCookies') {
-		chrome.cookies.remove({name:"yay", url:"http://exhentai.org/"}, function(){});
-		chrome.cookies.remove({name:"ipb_anonlogin", url:"http://exhentai.org/"}, function(){});
-		chrome.cookies.remove({name:"ipb_member_id", url:"http://exhentai.org/"}, function(){});
-		chrome.cookies.remove({name:"ipb_pass_hash", url:"http://exhentai.org/"}, function(){});
-		chrome.cookies.remove({name:"ipb_session_id", url:"http://exhentai.org/"}, function(){});
+		chrome.cookies.remove({name:"yay", url:"https://exhentai.org/"}, function(){});
+		chrome.cookies.remove({name:"ipb_anonlogin", url:"https://exhentai.org/"}, function(){});
+		chrome.cookies.remove({name:"ipb_member_id", url:"https://exhentai.org/"}, function(){});
+		chrome.cookies.remove({name:"ipb_pass_hash", url:"https://exhentai.org/"}, function(){});
+		chrome.cookies.remove({name:"ipb_session_id", url:"https://exhentai.org/"}, function(){});
 		chrome.cookies.remove({name:"ipb_anonlogin", url:"http://e-hentai.org/"}, function(){});
 		chrome.cookies.remove({name:"ipb_member_id", url:"http://e-hentai.org/"}, function(){});
 		chrome.cookies.remove({name:"ipb_pass_hash", url:"http://e-hentai.org/"}, function(){});
